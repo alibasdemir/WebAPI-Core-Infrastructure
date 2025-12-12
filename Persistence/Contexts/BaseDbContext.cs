1c998db30e22d6d581518cc4abfa48a7b32b8735
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-13 01:01:16 +0300

    Add BaseDbContext and TestRepository, remove Class1.cs
    Added BaseDbContext with Test DbSet and TestRepository for Test entity persistence. Set up project reference to Domain.csproj. Removed unused Class1.cs file.

---
+++ Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(""); // Connection string should be provided here.
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Persistence/Repositories/TestRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.DataAccess;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories
+{
+    public class TestRepository : EfRepositoryBase<Test, BaseDbContext>
+    {
+        public TestRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
