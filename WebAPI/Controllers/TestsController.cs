12497bfdb35ecbcc3a08b9930a938fc4b51dfff3
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-13 04:34:31 +0300

    Refactor controller IMediator injection via BaseController
    Introduce BaseController to provide IMediator using the Service Locator pattern, removing the need for constructor injection in child controllers like TestsController. This reduces boilerplate and simplifies controller code.

--- WebAPI/Controllers/TestsController.cs
+++ WebAPI/Controllers/TestsController.cs
@@ -1,20 +1,12 @@
 ﻿using Application.Features.Tests.Commands.Create;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TestsController : ControllerBase
+    public class TestsController : BaseController
     {
-        private readonly IMediator _mediator;
-
-        public TestsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpPost]
         public async Task<IActionResult> Add([FromBody] CreateTestCommand command)
         {
