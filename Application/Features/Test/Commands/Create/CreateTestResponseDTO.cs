7ef39898abba6d90c67c38e707b0d2e8dfe3f8c9
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-13 03:44:02 +0300

    Add MediatR setup and CreateTestCommand scaffolding
    Introduce MediatR to the Application layer with a new ApplicationServiceRegistration class and register it in Program.cs. Add CreateTestCommand and its handler (not yet implemented) along with a response DTO. Clean up .csproj by removing explicit folder includes for test features.

---
+++ Application/Features/Test/Commands/Create/CreateTestResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.Test.Commands.Create
+{
+    public class CreateTestResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
