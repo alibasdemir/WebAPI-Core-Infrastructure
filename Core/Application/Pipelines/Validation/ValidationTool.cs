f5393d3fd8740bf394c9e4b338c866b2a6b2b33f
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-28 02:36:11 +0300

    Add MediatR validation pipeline with FluentValidation
    Introduced ValidationBehavior to enable automatic request validation via FluentValidation in the MediatR pipeline. Registered validators and the behavior in ApplicationServiceRegistration. Added CreateTestCommandValidator for input rules and a ValidationTool for manual validation scenarios. This ensures consistent and centralized validation across the application.

---
+++ Core/Application/Pipelines/Validation/ValidationTool.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace Core.Application.Pipelines.Validation
+{
+    // For manual use WITHOUT MediatR (old method)
+    public class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            ValidationContext<object> context = new(entity);
+            ValidationResult result = validator.Validate(context);
+            if (!result.IsValid)
+                throw new ValidationException(result.Errors);
+        }
+    }
+
+    // Usage Example:
+    //public class SomeService
+    //{
+    //    public void CreateUser(CreateUserRequest request)
+    //    {
+    //        // Manuel Validation
+    //        ValidationTool.Validate(new CreateUserValidator(), request);
+
+    //        // Business Logic...
+    //    }
+    //}
+}
