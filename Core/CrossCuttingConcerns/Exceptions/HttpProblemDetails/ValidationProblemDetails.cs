4092ef62d9a7694ac2130826e838bdfc15509b66
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-28 04:17:56 +0300

    Add custom HTTP problem details classes for API errors
    Introduced a set of custom ProblemDetails classes under Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails to standardize API error responses. Includes specific classes for authorization, business, validation, not found, and internal server errors, each setting appropriate HTTP status codes and error details. ValidationProblemDetails also supports detailed validation error reporting. This improves consistency and clarity in API error handling.

---
+++ Core/CrossCuttingConcerns/Exceptions/HttpProblemDetails/ValidationProblemDetails.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails
+{
+    public class ValidationProblemDetails : ProblemDetails
+    {
+        public IEnumerable<ValidationExceptionModel> Errors { get; init; }
+        public ValidationProblemDetails(IEnumerable<ValidationExceptionModel> errors)
+        {
+            Errors = errors;
+            Title = "Validation error(s)";
+            Detail = "One or more validation errors occurred.";
+            Type = "ValidationException";
+            Status = StatusCodes.Status400BadRequest;
+        }
+    }
+}
