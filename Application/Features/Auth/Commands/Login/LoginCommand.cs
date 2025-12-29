36b9eba0f11015d1ef86e86c1c6d2dd799df3e74
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-29 19:33:59 +0300

    Add LoginCommand with handler for user authentication
    Introduced LoginCommand and LoginCommandHandler using MediatR to handle user login requests. The handler retrieves the user by email, verifies the password using HashingSaltingHelper, and throws a BusinessException if authentication fails. Added necessary dependency imports.

---
+++ Application/Features/Auth/Commands/Login/LoginCommand.cs
@@ -0,0 +1,36 @@
+﻿using Application.Repositories;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Core.Security.Entities;
+using Core.Security.HashingSalting;
+using MediatR;
+
+namespace Application.Features.Auth.Commands.Login
+{
+    public class LoginCommand : IRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public class LoginCommandHandler : IRequestHandler<LoginCommand>
+        {
+            private readonly IUserRepository _userRepository;
+
+            public LoginCommandHandler(IUserRepository userRepository)
+            {
+                _userRepository = userRepository;
+            }
+            public async Task Handle(LoginCommand request, CancellationToken cancellationToken)
+            {
+                User? user = await _userRepository.GetAsync(u => u.Email == request.Email);
+
+                if (user is null)
+                    throw new BusinessException("Login failed");
+
+                bool isPasswordMatch = HashingSaltingHelper.VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt);
+
+                if (!isPasswordMatch)
+                    throw new BusinessException("Login failed");
+            }
+        }
+    }
+}
