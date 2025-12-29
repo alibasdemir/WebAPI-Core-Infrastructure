825fa64c037f8186cf4a33c3b6fa78438657ee61
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-29 22:18:43 +0300

    Add JWT-based login endpoint and return access tokens
    Updated LoginCommand and handler to generate JWT access tokens upon successful authentication. Added a new "Login" POST endpoint in AuthsController to handle user login and return the token. Also explicitly mapped the Register endpoint.

--- WebAPI/Controllers/AuthsController.cs
+++ WebAPI/Controllers/AuthsController.cs
@@ -1,17 +1,25 @@
-﻿using Application.Features.Auth.Commands.Register;
+﻿using Application.Features.Auth.Commands.Login;
+using Application.Features.Auth.Commands.Register;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    [ApiController]         
     public class AuthsController : BaseController
     {
-        [HttpPost]
+        [HttpPost("Register")]
         public async Task<IActionResult> Add([FromBody] RegisterCommand registerCommand)
         {
             RegisterResponseDTO registerResponse = await _mediator.Send(registerCommand);
             return Ok(registerResponse);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginCommand loginCommand)
+        {
+            var loginResponse = await _mediator.Send(loginCommand);
+            return Ok(loginResponse);
         }
     }
 }
