eb6a6ea10ddd55fe1e5c9d1615f6642531bab3d1
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-29 18:31:59 +0300

    Add user registration endpoint with DTOs and mapping
    Implemented user registration flow with RegisterResponseDTO for structured responses. Updated RegisterCommand handler to return DTO using AutoMapper. Added MappingProfile for entity-DTO mapping. Introduced AuthsController with POST endpoint for registration.

---
+++ Application/Features/Auth/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.Auth.Commands.Register;
+using Application.Features.Tests.Commands.Create;
+using AutoMapper;
+using Core.Security.Entities;
+
+namespace Application.Features.Auth.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterCommand, User>()
+               .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTime.UtcNow));
+
+            CreateMap<User, RegisterResponseDTO>();
+        }
+    }
+}
