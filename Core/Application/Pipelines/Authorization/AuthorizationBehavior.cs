43ebcc60b4c25ec13196642a1c810bda960aa3ae
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-29 23:05:35 +0300

    Add MediatR authorization pipeline behavior
    Introduced AuthorizationBehavior to enforce authentication via MediatR pipeline. Registered the behavior in ApplicationServiceRegistration. Removed manual authentication checks from TestsController, delegating authorization to the new pipeline behavior.

---
+++ Core/Application/Pipelines/Authorization/AuthorizationBehavior.cs
@@ -0,0 +1,25 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Application.Pipelines.Authorization
+{
+    public class AuthorizationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public AuthorizationBehavior(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+                throw new AuthorizationException("You are not logged in");
+
+            TResponse response = await next();
+            return response;
+        }
+    }
+}
