57677681bd7d820776914e072e90b97bb5b0dc33
alibasdemir <basdemir35@hotmail.com.tr>
2025-12-29 17:37:18 +0300

    Add RegisterCommand and remove UserName from User entity
    Implemented RegisterCommand with handler for user registration using MediatR and AutoMapper. Removed the UserName property from the User entity. Added required using directives and repository integration.

---
+++ Application/Features/Auth/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,37 @@
+﻿using Application.Repositories;
+using AutoMapper;
+using Core.Security.Entities;
+using MediatR;
+
+namespace Application.Features.Auth.Commands.Register
+{
+    public class RegisterCommand : IRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public class RegisterCommandHandler : IRequestHandler<RegisterCommand>
+        {
+            private IUserRepository _userRepository;
+            private IMapper _mapper;
+
+            public RegisterCommandHandler(IUserRepository userRepository, IMapper mapper)
+            {
+                _userRepository = userRepository;
+                _mapper = mapper;
+            }
+
+            public async Task Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                User user = _mapper.Map<User>(request);
+
+                user.PasswordHash = null;
+                user.PasswordSalt = null;
+
+                await _userRepository.AddAsync(user);
+            }
+    }
+}
+}
