1d18a506d5af93f245ffaf87194fcb26c39d7894
alibasdemir <basdemir35@hotmail.com.tr>
2026-01-02 00:42:56 +0300

    Add repositories for OperationClaim and UserOperationClaim
    Introduce interfaces and implementations for OperationClaim and UserOperationClaim repositories. Register them in the DI container to support authorization-related data access.

---
+++ Application/Repositories/IUserOperationClaimRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.DataAccess;
+using Core.Security.Entities;
+
+namespace Application.Repositories
+{
+    public interface IUserOperationClaimRepository : IAsyncRepository<UserOperationClaim>, IRepository<UserOperationClaim>
+    {
+    }
+}
